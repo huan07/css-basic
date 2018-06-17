0d51c7bcfd71e3b1e9e35eec9f37955e248da047
huan07 <845265003@qq.com>
2018-06-17 16:47:21 +0800

    .

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,36 @@ var del = require('del');
 var gulp = require('gulp');
 var less = require('gulp-less');
 
-gulp.task('css', function(cb) {
+gulp.task('css', function(cb){
     return del('css/*', cb);
 });
 
-gulp.task('less', ['css'], function() {
+gulp.task('less', ['css'], function(){
     return gulp.src('less/*.less')
         .pipe(less())
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('image', function() {
+gulp.task('image', function(){
     return gulp.src('less/image/*')
         .pipe(gulp.dest('css/image'));
 });
 
-gulp.task('default', ['less', 'image']);
+
+gulp.task('custom-practice-css', function(cb){
+    return del('custom-practice-css/*', cb);
+});
+
+gulp.task('custom-practice-less', ['custom-practice-css'], function(){
+    return gulp.src('custom-practice-less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('custom-practice-css'));
+});
+
+gulp.task('custom-practice-image', function(){
+    return gulp.src('custom-practice-less/image/*')
+        .pipe(gulp.dest('custom-practice-css/image'));
+});
+
+
+gulp.task('default', ['less', 'image', 'custom-practice-less', 'custom-practice-image']);
